c99360a71a27e8ca510b0829675e15ecaca2e989
magicbart <magicbart@gmail.com>
2025-12-08 21:50:36 +0100

    Add i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,32 @@ export default defineNuxtConfig({
   modules: [
       '@nuxtjs/tailwindcss',
       '@pinia/nuxt',
+      '@nuxtjs/i18n',
   ],
+  i18n: {
+    locales: [
+      {
+        code: 'fr',
+        iso: 'fr-FR',
+        name: 'Français',
+        file: 'fr.json'
+      },
+      {
+        code: 'en',
+        iso: 'en-US', 
+        name: 'English',
+        file: 'en.json'
+      }
+    ],
+    langDir: 'locales',
+    defaultLocale: 'en',
+    strategy: 'no_prefix',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root'
+    }
+  },
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.API_BASE_URL || 'http://backoffice.vgr.local/api'
